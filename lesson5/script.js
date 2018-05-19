c4e59aed9ab637a192cec4e28011969d680c4ced
Your Name <you@example.com>
2018-05-19 16:36:53 +0400

    Del-i avelacum

---
+++ lesson5/script.js
@@ -0,0 +1,35 @@
+function main() {
+    var socket = io.connect('http://localhost:3000');
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var buttondel = document.getElementById('Del');
+
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+    }
+
+
+
+    //del msg
+    /*function handleMessage(msg) {
+        var p = document.createElement('p');
+        msg = [];
+    }
+    socket.on('display message');*/
+
+
+    socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;
